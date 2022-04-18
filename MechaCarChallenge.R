967c7a5a9a36e429f06b2072c603e96a0affe8e9
Yesenia Perez Ibarra <83614893+yempi@users.noreply.github.com>
2022-04-17 21:24:00 -0500

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -30,5 +30,25 @@ total_summary <- Suspension %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=v
 lot_summary <-Suspension %>% group_by(Manufacturing_Lot)%>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
 
 
+# **********************************************************************************************************************************
 
 
+# Deliverable 3: T-tests on Suspension Coils 
+# ---------------------------------------------------
+# determine with t.test() if the PSI across all manufacturing lots is statistically different 
+# from the population mean of 1500 pounds per in2
+
+# All lots
+t.test(Suspension$PSI,mu=1500)
+
+# determine with subset() if the PSI across all manufacturing lots is statistically different 
+# from the population mean of 1500 pounds per in2
+
+# Lot 1
+t.test(subset(Suspension,Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+
+# Lot 2
+t.test(subset(Suspension,Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+ 
+# Lot 3
+t.test(subset(Suspension,Manufacturing_Lot=="Lot3")$PSI,mu=1500)
